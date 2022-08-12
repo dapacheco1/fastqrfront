81f5609f6c5bfee6c3d7d4be6e608a4f9d04855b
Danny Pacheco <dapacheco1@espe.edu.ec>
2022-08-11 19:14:13 -0500

    formulario para desaparecido

---
+++ src/pages/FormDesaparecido.js
@@ -0,0 +1,33 @@
+const FormDesaparecido = () =>{
+    
+    const view = `
+    <!-- Form del objeto o persona perdida -->
+        <div class="form" id="objPerdido">
+            <h2>Datos de Persona o Mascota Propenso a Desaparecer</h2>
+            <div class="mb-3">
+                <label for="nombresD">Nombres: </label>
+                <input type="text" name="nombresD" class="form-control" >
+            </div>
+            <div class="mb-3">
+                <a href="#/contacto/" class="btn btn-primary preview mt-2"><i class="fa-solid fa-angle-left"></i> Atras </a>
+                <button type="button" class="btn btn-success convertBtn mt-2" style="display:none"> Generar codigo QR <i class="fa-solid fa-qrcode"></i></button>
+                <button type="button" class="btn btn-success saveDisappear mt-2">Guardar Datos Desaparecido <i class="fa-solid fa-floppy-disk"></i></button>
+              </div>
+            <div class="mb-3">
+             
+                <div class="d-flex justify-content-center">
+
+                  <div class="result" style="display:none"></div>
+                </div>
+             
+            </div>
+            <div class="mb-3">
+            
+            </div>
+           
+        </div>
+    `;
+    return view;
+};
+
+export {FormDesaparecido};
