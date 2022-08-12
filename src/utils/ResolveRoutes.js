8bf230ef279850401e4ea42cd490f1f73390ac7d
Danny Pacheco <dapacheco1@espe.edu.ec>
2022-08-11 21:23:44 -0500

    nueva logica para resolver rutas

--- src/utils/ResolveRoutes.js
+++ src/utils/ResolveRoutes.js
@@ -1,7 +1,10 @@
 const ResolveRoutes = (route)=>{
-    if(route === '/'){
+    let token = localStorage.getItem("token")?localStorage.getItem("token"):undefined;
+    if(route === '/' && token!=undefined){
         return route;
-    }else{
+    }else if(token==undefined){
+        return `/login`;
+    }else if(token!=undefined){
 
         return `/${route}`;
     }
