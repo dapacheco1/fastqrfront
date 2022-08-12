ab820840946ed4cd60de8f2e4b4a91ac848fb11a
Danny Pacheco <dapacheco1@espe.edu.ec>
2022-08-11 21:20:40 -0500

    actualizacion form desaparecido

--- src/pages/FormDesaparecido.js
+++ src/pages/FormDesaparecido.js
@@ -1,16 +1,18 @@
 const FormDesaparecido = () =>{
-    
+    alert("Una vez generado el codigo QR se resetea los campos del formulario.");
+    let nombresD = localStorage.getItem("nombresD")?localStorage.getItem("nombresD"):"";
+
     const view = `
     <!-- Form del objeto o persona perdida -->
         <div class="form" id="objPerdido">
             <h2>Datos de Persona o Mascota Propenso a Desaparecer</h2>
             <div class="mb-3">
                 <label for="nombresD">Nombres: </label>
-                <input type="text" name="nombresD" class="form-control" >
+                <input type="text" name="nombresD" class="form-control" value="${nombresD}">
             </div>
             <div class="mb-3">
                 <a href="#/contacto/" class="btn btn-primary preview mt-2"><i class="fa-solid fa-angle-left"></i> Atras </a>
-                <button type="button" class="btn btn-success convertBtn mt-2" style="display:none"> Generar codigo QR <i class="fa-solid fa-qrcode"></i></button>
+                <button type="button" class="btn btn-success convertBtn mt-2"> Generar codigo QR <i class="fa-solid fa-qrcode"></i></button>
                 <button type="button" class="btn btn-success saveDisappear mt-2">Guardar Datos Desaparecido <i class="fa-solid fa-floppy-disk"></i></button>
               </div>
             <div class="mb-3">
