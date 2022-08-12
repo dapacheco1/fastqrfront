7b9902bbe97ed7738dd51f4bdb12d79b51921d5d
Danny Pacheco <dapacheco1@espe.edu.ec>
2022-08-11 21:23:20 -0500

    update post with try-catch and validations

--- src/utils/Post.js
+++ src/utils/Post.js
@@ -1,17 +1,34 @@
-const Post = (url, data,header) =>{
+const Post = async (url, data,header) =>{
     let id = 0;
-    fetch(url, {
-      method: "POST",
-      mode: "cors",
-      headers: header,
-      body: JSON.stringify(data),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        alert(data.message);
-        id = data.data;
-        console.log(data);
+    let dataResponse = 0;
+    try{
+      let response = await fetch(url, {
+        method: "POST",
+        mode: "cors",
+        headers: header,
+        body: JSON.stringify(data),
       });
+    
+      alert("Procesando solicitud, Espere por favor...");
+    
+      dataResponse = await response.json();
+      
+      id = dataResponse.data;
+      if(id==0){
+        alert("Error al guardar los datos.Intente guardar nuevamente.");
+      }else{
+        alert(dataResponse.message);
+      }
+      
+    }catch(e){
+      alert("Error al guardar datos. Favor contacte con soporte tecnico.");
+      console.group("Error al momento de procesar solicitud metodo POST");
+      console.error(e);
+      console.groupEnd();
+    }
+    
+    return id;
+    
 };
 
 export {Post};
