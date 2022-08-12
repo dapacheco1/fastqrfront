75d6097911044714aaf0a6441528944e9a88852d
Danny Pacheco <dapacheco1@espe.edu.ec>
2022-08-11 19:14:55 -0500

    pagina de soporte tecnico

---
+++ src/pages/Support.js
@@ -0,0 +1,11 @@
+const Support = ()=>{
+    const view = `
+        <h5>Si tiene preguntas acerca del funcionamiento de la plataforma, contacte a la siguiente
+            dirrecion de correo <a href="#">dannypachecodev@hotmail.com</a> o al número de teléfono 
+            <a href="tel:0979379671">0979379671</a>
+        </h5>
+    `;
+    return view;
+};
+
+export {Support};
